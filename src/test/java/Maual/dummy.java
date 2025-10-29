81280a57a3a0273f7d6684dd401ab669f0e265af
magendran07 <magendranmagi2008@gmail.com>
2025-10-29 12:15:17 +0530

    commit3

--- src/test/java/Maual/dummy.java
+++ src/test/java/Maual/dummy.java
@@ -3,5 +3,8 @@ package Maual;
 public class dummy {
 public static void main(String[] args) {
 	System.out.println("Bye Bye");
+	System.out.println("Khatam");
+	System.out.println("By eBye");
+	System.out.println("nue");
 }
 }
